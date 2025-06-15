147aab3309e9d3846245714afba42d698346ab34
erytym <protapruddrodeb@gmail.com>
2025-06-15 13:34:03 +0600

    Initial commit - Angular restaurant website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
+import { FeaturesComponent } from "./components/features/features.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ HeroComponent, FeaturesComponent, MenuComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
